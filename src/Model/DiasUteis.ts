35fd9fedfd660c669a336b50c3028667baa41b91
edusouza96 <eduardo_souza96@hotmail.com>
2022-05-22 14:54:30 -0300

    classe abstratara para os dias uteis, com tratamento de ingresso com carteirinha

---
+++ src/Model/DiasUteis.ts
@@ -0,0 +1,31 @@
+import { IDesconto } from "../Interfaces/IDesconto";
+import { IIngresso } from "../Interfaces/IIngresso";
+import { Estudante } from "./Estudante";
+
+export abstract class DiasUteis implements IDesconto
+{
+    protected ingresso: IIngresso;
+    protected valor: number = 0;
+
+    constructor(ingresso: IIngresso)
+    {
+        this.ingresso = ingresso;
+    }
+
+    getValor()
+    {
+        if(this.isEstudanteComCarteirinha()) 
+            this.valor = this.getValoEstudanteComCarteirinha();
+
+        return this.valor;
+    }
+
+    protected isEstudanteComCarteirinha(): boolean
+    {
+        return (this.ingresso instanceof Estudante && this.ingresso.hasCarteirinha());
+    }
+    protected getValoEstudanteComCarteirinha(): number
+    {
+        return 35;
+    }
+}
